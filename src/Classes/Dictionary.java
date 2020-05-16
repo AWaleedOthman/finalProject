3d235e113afe3c5e3188a7e347b5aed80b9b5253
Ahmad Waleed <a.waleedothman@gmail.com>
2020-05-16 12:06:31 +0200

    Initial commit

---
+++ src/Classes/Dictionary.java
@@ -0,0 +1,25 @@
+package Classes;
+
+import Interfaces.IDictionary;
+
+public class Dictionary<K,V> implements IDictionary<K,V> {
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public V set(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
